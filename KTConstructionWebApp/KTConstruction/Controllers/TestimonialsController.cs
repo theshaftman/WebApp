800aff9a84240e82928e04255790d506211f2c60
theshaftman <mariyan_apostolov@mail.bg>
2017-10-05 07:53:12 +0300

    Gallery initial inclusion

---
+++ KTConstructionWebApp/KTConstruction/Controllers/TestimonialsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KTConstruction.Controllers
+{
+    public class TestimonialsController : Controller
+    {
+        // GET: Testimonials
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
