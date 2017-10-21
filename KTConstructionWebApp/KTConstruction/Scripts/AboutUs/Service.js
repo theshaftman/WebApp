892e8aa8a77a86a95e839146d86b8c81a08dc6d1
theshaftman <mariyan_apostolov@mail.bg>
2017-10-21 20:33:42 +0300

    Initialize AboutUs Page #5

---
+++ KTConstructionWebApp/KTConstruction/Scripts/AboutUs/Service.js
@@ -0,0 +1,24 @@
+﻿var Service = (function () {
+    var self = this;
+    self.ajaxRequest = function (requestType, url, data) {
+        return $.ajax({
+            type: requestType,
+            url: url,
+            dataType: "json",
+            data: data
+        });
+    };
+    self.projectNumbers = {
+        allProjects: 18,
+        currentProjects: 9,
+        interiorRepairs: 7,
+        exteriorRepairs: 5,
+        bathRepairs: 6
+    };
+    self.setIntervalTime = 70;
+    return {
+        ajaxRequest: self.ajaxRequest,
+        projectNumbers: self.projectNumbers,
+        setIntervalTime: self.setIntervalTime
+    };
+}());
---
+++ KTConstructionWebApp/KTConstruction/Scripts/AboutUs/ViewModel.js
@@ -0,0 +1,27 @@
+﻿var ViewModel = function () {
+    var self = this;
+    self.loadPage = function () {
+        //$('#sightseeing').waypoint(function (direction) {
+        //    alert("visible");
+        //});
+        var numbers = Service.projectNumbers;
+        loadNumbers("allProjects", numbers.allProjects);
+        loadNumbers("interiorRepairs", numbers.interiorRepairs);
+        loadNumbers("exteriorRepairs", numbers.exteriorRepairs);
+        loadNumbers("bathRepairs", numbers.bathRepairs);
+        loadNumbers("currentProjects", numbers.currentProjects);
+    }
+
+    function loadNumbers(givenId, givenMaxNumber) {
+        var dfd = $.Deferred();
+        var currentNumber = 0;
+        var currentInterval = setInterval(function () {
+            if (currentNumber == givenMaxNumber) {
+                clearInterval(currentInterval);
+            }
+            document.getElementById(givenId).innerText = currentNumber
+            currentNumber += 1;
+        }, Service.setIntervalTime);
+        return dfd.resolve(true);
+    }
+};
